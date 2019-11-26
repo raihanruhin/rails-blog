6d0055bca742fea6ad77474bdc0407f19f15c0b1
raihanruhin <raihanruhin@gmail.com>
2019-11-26 18:18:18 +0900

    implemented destroy feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,6 +16,14 @@ class PortfoliosController < ApplicationController
         @portfolio_item = Portfolio.find(params[:id])
     end
 
+    def destroy
+        @portfolio_item = Portfolio.find(params[:id])
+        @portfolio_item.destroy
+        respond_to do |format|
+          format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
+        end
+    end
+
     def create
         @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
