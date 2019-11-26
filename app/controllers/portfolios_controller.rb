591f1e76814ca80379541d02e5ce3676b37e2882
raihanruhin <raihanruhin@gmail.com>
2019-11-26 17:46:57 +0900

    create and edit portfolio in home

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,10 @@ class PortfoliosController < ApplicationController
          @portfolio_item = Portfolio.new
     end
 
+    def edit
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
     def create
         @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -19,4 +23,17 @@ class PortfoliosController < ApplicationController
             end
         end
     end
+
+    def update
+        @portfolio_item = Portfolio.find(params[:id])
+
+        respond_to do |format|
+          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+          else
+            format.html { render :edit }
+          end
+        end
+      end
+
 end
