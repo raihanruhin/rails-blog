b40764f1bdc355273bd577f52d0b4b415484c088
raihanruhin <raihanruhin@gmail.com>
2019-11-26 15:37:06 +0900

    databases seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+    Blog.create!(
+        title: "Blog Post #{blog}",
+        body: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections"
+    )
+end
+puts "Blog posts created"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}", 
+        percent_utilized: 15
+    )
+end
+puts "Skill created"
+
+9.times do |portfolio|
+    Portfolio.create!(
+        title: "Portfolio title #{portfolio}", 
+        subtitle: "some title here", 
+        body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.", 
+        main_image: "https://via.placeholder.com/600x200",
+        thumb_image: "https://via.placeholder.com/350x200"
+    )
+end
+puts "portfolio item created"
